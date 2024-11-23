bc8896e070fcb637f7c2928ced384049da450af3
Ahmetcapar1 <100695576+Ahmetcapar1@users.noreply.github.com>
2024-11-23 22:56:02 +0300

    oldu

--- streamlitapp.py
+++ streamlitapp.py
@@ -12,13 +12,13 @@ st.sidebar.header("Filters")
 
 start_date = st.sidebar.date_input("Start Date", value=data['date'].min())
 end_date = st.sidebar.date_input("End Date", value=data['date'].max())
-flag_filter = st.sidebar.multiselect("Flag", options=data["flag"].dropna().unique())
+category_filter = st.sidebar.multiselect("Category", options=data["category"].unique())
 scope_filter = st.sidebar.multiselect("Scope", options=data["scope"].dropna().unique())
 
 filtered_data = data[
     (data['date'] >= pd.Timestamp(start_date)) &
     (data['date'] <= pd.Timestamp(end_date)) &
-    (data['flag'].isin(flag_filter)) &
+    (data['category'].isin(category_filter)) &
     (data['scope'].isin(scope_filter))
 ]
 
