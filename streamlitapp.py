8d5441aecf34265c9d0fae9d31d436602215e42a
Ahmetcapar1 <100695576+Ahmetcapar1@users.noreply.github.com>
2024-11-23 23:29:33 +0300

    update

--- streamlitapp.py
+++ streamlitapp.py
@@ -8,13 +8,26 @@ data_path = "data.csv"
 data = pd.read_csv(data_path)
 data['date'] = pd.to_datetime(data['date'])
 
+drivers_file = "F1Drivers.csv"
+drivers_data = pd.read_csv(drivers_file)
+
+new_data = pd.merge(
+    data, 
+    drivers_data[['driver_number', 'name_acronym']],  
+    on='driver_number', 
+    how='left'
+)
+
+data['driver'] = new_data['name_acronym'] 
+
 st.sidebar.header("Filters")
 
 start_date = st.sidebar.date_input("Start Date", value=data['date'].min())
 end_date = st.sidebar.date_input("End Date", value=data['date'].max())
-category_filter = st.sidebar.multiselect("Category", options=data["category"].unique())
-flag_filter = st.sidebar.multiselect("Flag", options=data["flag"].dropna().unique())
-scope_filter = st.sidebar.multiselect("Scope", options=data["scope"].dropna().unique())
+category_filter = st.sidebar.multiselect("Category", options=data["category"].unique(),default=data["category"].unique())
+flag_filter = st.sidebar.multiselect("Flag", options=data["flag"].dropna().unique(),default=data["flag"].unique())
+scope_filter = st.sidebar.multiselect("Scope", options=data["scope"].dropna().unique(),default=data["scope"].unique())
+driver_filter = st.sidebar.multiselect("Driver",options=data["driver"].unique(),default=data["driver"].unique())
 
 if st.sidebar.button("Apply Filters"):
     filtered_data = data[
@@ -22,7 +35,8 @@ if st.sidebar.button("Apply Filters"):
         (data['date'] <= pd.Timestamp(end_date)) &
         (data['category'].isin(category_filter) if category_filter else True) &
         (data['flag'].isin(flag_filter) if flag_filter else True) &
-        (data['scope'].isin(scope_filter) if scope_filter else True)
+        (data['scope'].isin(scope_filter) if scope_filter else True) &
+        (data["driver"].isin(driver_filter) if driver_filter else True)
     ]
 
     st.header("Filtered Events")
